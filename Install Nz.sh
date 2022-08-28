c565590846624e1532658ae4633b2263d11fef3a
NAZMUL HOSSAIN <112247853+NazmulHossain1623@users.noreply.github.com>
2022-08-28 10:59:59 +0600

    Create Install Nz.sh

---
+++ Install Nz.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#code_recorded_by_nazmul_hossain
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+
+███╗   ██╗ █████╗ ███████╗███╗   ███╗██╗   ██╗██╗     
+████╗  ██║██╔══██╗╚══███╔╝████╗ ████║██║   ██║██║     
+██╔██╗ ██║███████║  ███╔╝ ██╔████╔██║██║   ██║██║     
+██║╚██╗██║██╔══██║ ███╔╝  ██║╚██╔╝██║██║   ██║██║     
+██║ ╚████║██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝███████╗
+╚═╝  ╚═══╝╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝
+                                                      
+" | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip techcochi.zip
+clear
+rm -rf techcochi.zip
+clear
+bash about.sh
+sleep 4
+echo ""
+echo ""
+echo " FOLLOW MY FACEBOOK ACCOUNT 🔔 " | lolcat
+xdg-open https://www.facebook.com/NazmulHossain16239
+echo ""
+sleep 10
+echo " FOLLOW OUR PAGE" | lolcat
+echo " Loading our page.... "
+termux-open-url https://www.facebook.com/profile.php?id=100043316096491
+echo ""
